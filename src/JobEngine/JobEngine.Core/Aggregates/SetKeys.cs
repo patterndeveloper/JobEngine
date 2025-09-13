fd1be13ebed67aa43a9dae50cd78da89649f61e1
Ebrahim Khani <ebrahim.khani@outlook.com>
2025-09-13 16:31:27 +0000

    Create aggregates

---
+++ src/JobEngine/JobEngine.Core/Aggregates/SetKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace JobEngine.Core.Aggregates;
+
+public class SetKeys
+{
+    public const string Schedule = "schedule";
+}
---
+++ src/JobEngine/JobEngine.Core/Aggregates/State.cs
@@ -0,0 +1,11 @@
+﻿namespace JobEngine.Core.Aggregates;
+
+public class State
+{
+    public long JobId { get; set; }
+    public long Id { get; set; }
+    public JobState JobState { get; set; }
+    public string? Reason { get; set; }
+    public string? Data { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
